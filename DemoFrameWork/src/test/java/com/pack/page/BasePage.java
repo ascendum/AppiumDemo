f0d9dce2504c4b9d9943e02ba1178f40d138ceff
chinmayaasc <chinmaya.satapathy@ascendum.com>
2016-03-11 17:37:26 +0530

    logger commit

--- DemoFrameWork/src/test/java/com/pack/page/BasePage.java
+++ DemoFrameWork/src/test/java/com/pack/page/BasePage.java
@@ -34,7 +34,7 @@ public class BasePage {
 	
 	public String getPageTitle(){
 		String title = driver.getTitle();
-		logger.info("title captured");
+		logger.info("title captured1");
 		return title;
 	}
 	
